5fbdaf30b836adb37456735bd9da9f826128db91
muflihmalih <muflih150@gmail.com>
2020-11-05 21:38:59 +0700

    signup frontend

--- src/container/login.js
+++ src/container/login.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
+import Axios from "axios";
 
-const login = () => {
+const Login = () => {
+  const [username,setUsername]=useState("")
+  const [password,setPassword]=useState("")
+
+  const onSubmit = (e) =>{
+    e.preventDefault()
+    //alert(username+password)
+    // if(!username || !password){
+    //   return alert("harus mengisi username dan password")
+    // }
+    Axios.post('http://localhost:8000/api/login', {
+      username,
+      password
+    })
+    .then(function (response) {
+      alert("succes")
+      console.log (response.data)
+    })
+    .catch(function (error) {
+      alert("error")
+    });
+  }
+
   return (
     <div>
-      <h1>login</h1>
+     <h1>login</h1>
+      <form onSubmit={onSubmit}>
+        <label>
+          username
+        </label>
+        <input value={username} onChange={e=>setUsername(e.target.value)} required/>
+        <label>
+          password
+        </label>
+        <input value={password} onChange={e=>setPassword(e.target.value)} required/>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
-export default login;
+export default Login;
--- src/container/signup.js
+++ src/container/signup.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React,{useState} from "react";
+import Axios from "axios";
 
-const signup = () => {
+const Signup = () => {
+  const [username,setUsername]=useState("")
+  const [password,setPassword]=useState("")
+
+  const onSubmit = (e) =>{
+    e.preventDefault()
+    //alert(username+password)
+    Axios.post('http://localhost:8000/api/signup', {
+      username,
+      password
+    })
+    .then(function (response) {
+      alert("succes")
+      console.log (response.data)
+    })
+    .catch(function (error) {
+      alert("error")
+    });
+  }
+
+
+
   return (
     <div>
       <h1>signup</h1>
+      <form onSubmit={onSubmit}>
+        <label>
+          username
+        </label>
+        <input value={username} onChange={e=>setUsername(e.target.value)}/>
+        <label>
+          password
+        </label>
+        <input value={password} onChange={e=>setPassword(e.target.value)}/>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
-export default signup;
+export default Signup;
